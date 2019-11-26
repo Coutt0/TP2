1c32417e9f80887b67e7410fcad1c5580c26f610
Coutt0 <renatocoutinho28@gmail.com>
2019-11-26 00:52:41 +0000

    Finalização da classe main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String args[]) {
        Category category1 = new Category("Desktops","Gaming","Preto");
        Category category2 = new Category("Laptops","Trabalho","Branco");
-       Category category3 = new Category("Telemóveis","Iphone","Rosa");
+       Category category3 = new Category("Telemoveis","Iphone","Rosa");
 
        Product product1 = new Product("MSI","Trident","picture",category1,649,"un");
        Product product2 = new Product("HP","Pavilion","picture",category1,430,"un");
@@ -21,5 +21,60 @@ public class Main {
 
        User user1 = new User("Renato","renato@gmail.com","renato","password",address1);
        User user2 = new User("Tiago","tiago@gmail.com","TIAGO","ola",address2);
+       List<User> UserList = new ArrayList<>();
+       UserList.add(user1);
+       UserList.add(user2);
+
+       List<Product> newProductList1 = new ArrayList<>();
+               newProductList1.add(product1);
+               newProductList1.add(product2);
+               newProductList1.add(product3);
+               newProductList1.add(product4);
+               newProductList1.add(product5);
+               newProductList1.add(product6);
+               newProductList1.add(product7);
+
+       List<Product> newProductList2 = new ArrayList<>();
+              newProductList2.add(product6);
+              newProductList2.add(product1);
+
+       Shoppinglist list1=new Shoppinglist("Lista1",user1,newProductList1,newProductList2,UserList);
+
+
+       //Username das pessoas que partilham a lista
+       System.out.println("Lista Partilhada por:");
+       for (int i = 0;i<UserList.size();i++){
+           System.out.println(UserList.get(i).getUsername());
+       }
+       //Nº de produtos + total de produtos no carrinho
+        System.out.println("Produtos do carrinho de compras:");
+       for(int i = 0;i<newProductList2.size();i++){
+           System.out.println(newProductList2.get(i).getName());
+       }
+       System.out.println("Valor dos Produtos");
+       System.out.println(list1.GetTotalPriceOnCart()+"Euros");
+
+       //Nº de produtos + total de produtos ainda na lista
+        System.out.println(("Produtos que estão ainda na lista:"));
+        for (int i = 0;i<newProductList1.size();i++){
+            System.out.println((newProductList1.get(i).getName()));
+        }
+        System.out.println("Valor dos Produtos");
+        System.out.println(list1.GetTotalPrice()+"Euros");
+
+        //Percentagem
+        System.out.println("Percentage Complete:");
+        System.out.println(list1.GetPercentageCompleted());
+
+        //Categorias que existem no carrinho e por categoria o nome dos produtos
+        System.out.println("Categorias Carrinho compras");
+        for (int i = 0;i<newProductList2.size();i++){
+            System.out.println((newProductList2.get(i).getCategoryName()));
+        }
+        System.out.println(("Nome dos produtos das categorias no carrinho"));
+        for (int i = 0;i<newProductList2.size();i++){
+            System.out.println((newProductList2.get(i).getName()));
+        }
+
     }
 }
