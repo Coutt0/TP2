35a86df75d8b7b4a632c033bdbb7e718004c280b
Coutt0 <renatocoutinho28@gmail.com>
2019-12-02 22:53:20 +0000

    no message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,24 @@ public class Main {
        Category category2 = new Category("Laptops","Trabalho","Branco");
        Category category3 = new Category("Telemoveis","Iphone","Rosa");
 
-       Product product1 = new Product("MSI","Trident","picture",category1,649,"un");
-       Product product2 = new Product("HP","Pavilion","picture",category1,430,"un");
-       Product product3 = new Product("Lenovo","V145","picture",category2,300,"un");
-       Product product4 = new Product("Asus","Vivobook","picture",category2,350,"un");
-       Product product5 = new Product("iPhone7","32GB","picture",category3,500,"un");
-       Product product6 = new Product("iPhone8","32GB","picture",category3,550,"un");
-       Product product7 = new Product("iPhone11","256GB","picture",category3,1450,"un");
+       Product product1 = new Product("MSI","Trident","picture",category1,649,"un","msi");
+       Product product2 = new Product("HP","Pavilion","picture",category1,430,"un","hp");
+       Product product3 = new Product("Lenovo","V145","picture",category2,300,"un","asus");
+       Product product4 = new Product("Asus","Vivobook","picture",category2,350,"un","asus");
+       Product product5 = new Product("iPhone7","32GB","picture",category3,500,"un","apple");
+       Product product6 = new Product("iPhone8","32GB","picture",category3,550,"un","apple");
+       Product product7 = new Product("iPhone11","256GB","picture",category3,1450,"un","apple");
 
        Address address1 = new Address("Barcelos",345,"123-1414");
        Address address2 = new Address("Braga",527,"121-1424");
 
        User user1 = new User("Renato","renato@gmail.com","renato","password",address1);
        User user2 = new User("Tiago","tiago@gmail.com","TIAGO","ola",address2);
+       User user3 = new User("Joao","joao@gmail.com","Joao","ola1",address2);
        List<User> UserList = new ArrayList<>();
        UserList.add(user1);
        UserList.add(user2);
+       UserList.add(user3);
 
        List<Product> newProductList1 = new ArrayList<>();
                newProductList1.add(product1);
@@ -39,7 +41,7 @@ public class Main {
               newProductList2.add(product1);
 
        Shoppinglist list1=new Shoppinglist("Lista1",user1,newProductList1,newProductList2,UserList);
-
+       Shoppinglist list2=new Shoppinglist("Lista2",user1,newProductList1,newProductList2,UserList);
 
        //Username das pessoas que partilham a lista
        System.out.println("Lista Partilhada por:");
